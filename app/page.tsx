ac79c011090f9f8f0543875c91e03ad5e127d08e
ddolboghi <rhdfyd128@gmail.com>
2024-08-26 01:24:38 +0900

    fix: 메인페이지 html 구조 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@ import GoogleLoginButton from "@/components/GoogleLoginButton";
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <main className="flex-1 flex flex-col gap-6 px-4">
-          <GoogleLoginButton />
-        </main>
-      </div>
-    </div>
+    <main>
+      <GoogleLoginButton />
+    </main>
   );
 }
