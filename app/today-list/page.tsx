a2a25db293520c138fc5ebab5b63c1daee1295bd
ddolboghi <rhdfyd128@gmail.com>
2024-08-26 00:56:49 +0900

    feat: 로그인 여부에 따른 페이지 접근 제한

---
+++ app/today-list/page.tsx
@@ -0,0 +1,10 @@
+import AddCheckList from "@/components/AddCheckList";
+
+export default async function TodayList() {
+  return (
+    <div>
+      <h1>todo list</h1>
+      <AddCheckList />
+    </div>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,28 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { NextRequest, NextResponse } from "next/server";
+import { updateSession } from "./utils/supabase/middleware";
+import { createClient } from "./utils/supabase/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+  const { pathname } = request.nextUrl;
+
+  const {
+    data: { user },
+  } = await createClient().auth.getUser();
+
+  const protectedRoutes = ["/today-list"];
+
+  const isLoggedIn = user !== null;
+
+  if (protectedRoutes.includes(pathname) && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
