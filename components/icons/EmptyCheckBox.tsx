949d7f2149f4f87f72020cbbef84c1b883248152
ddolboghi <rhdfyd128@gmail.com>
2024-08-27 07:15:01 +0900

    feat: 체크박스 이미지 컴포넌트 추가

---
+++ components/icons/EmptyCheckBox.tsx
@@ -0,0 +1,15 @@
+function EmptyCheckBox() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect y="0.639282" width="18" height="17.3454" rx="4" fill="white" />
+    </svg>
+  );
+}
+
+export default EmptyCheckBox;
---
+++ components/icons/FillCheckBox.tsx
@@ -0,0 +1,22 @@
+function FillCheckBox() {
+  return (
+    <svg
+      width="18"
+      height="19"
+      viewBox="0 0 18 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect y="0.455933" width="18" height="18" rx="4" fill="#ABABAB" />
+      <path
+        d="M4.16663 9.6425L7.41663 13.215L13.9166 6.58032"
+        stroke="white"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default FillCheckBox;
