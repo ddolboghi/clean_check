409f2b58c11dd874b226661bc2b726392b60398f
ddolboghi <rhdfyd128@gmail.com>
2024-08-27 00:37:32 +0900

    refactor: 로그인한 유저 리다이렉팅 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,16 @@
 import GoogleLoginButton from "@/components/GoogleLoginButton";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const {
+    data: { user },
+  } = await createClient().auth.getUser();
+
+  if (user) {
+    redirect("/checklist");
+  }
+
   return (
     <div className="flex flex-col flex-1 p-4 w-full items-center">
       <GoogleLoginButton />
