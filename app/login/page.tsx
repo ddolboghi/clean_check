4cf8c85bfbeb8032a597f76547a7e6474531d603
ddolboghi <rhdfyd128@gmail.com>
2024-08-27 12:00:28 +0900

    style: 로그인 페이지 디자인 적용

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import GoogleLoginButton from "@/components/GoogleLoginButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import cleanFreeLogo from "@/assets/cleanfreeLogo2.png";
+import Image from "next/image";
 
 export default async function Login() {
   const {
@@ -12,8 +14,22 @@ export default async function Login() {
   }
 
   return (
-    <div className="flex flex-col flex-1 p-4 w-full items-center">
+    <main className="pb-[76px] flex flex-col gap-[177px] justify-end items-center h-screen overflow-hidden">
+      <div className="text-center">
+        <h1 className="font-semibold text-[40px] mb-6">환영합니다!</h1>
+        <p className="text-[#808080] text-[20px]">
+          스킨체크와 함께 피부를 지켜보세요.
+        </p>
+      </div>
+      <div>
+        <Image
+          src={cleanFreeLogo}
+          width={195}
+          height={195}
+          alt="클린프리 로고"
+        />
+      </div>
       <GoogleLoginButton />
-    </div>
+    </main>
   );
 }
--- components/GoogleLoginButton.tsx
+++ components/GoogleLoginButton.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { googleLogin } from "@/lib/googleLogin";
+import GoogleLogo from "./icons/GoogleLogo";
 
 const GoogleLoginButton = () => {
-  return <button onClick={() => googleLogin()}>Login with Google</button>;
+  return (
+    <div className="w-[354px] h-[60px] rounded-full items-center border-solid border-[#DADADA] border-2">
+      <GoogleLogo className="relative top-[50%] translate-y-[-50%] left-[15%]" />
+      <button
+        className="relative bottom-[50%] translate-y-[60%] text-[20px] left-[50%] translate-x-[-50%]"
+        onClick={() => googleLogin()}
+      >
+        구글로 로그인하기
+      </button>
+    </div>
+  );
 };
 
 export default GoogleLoginButton;
