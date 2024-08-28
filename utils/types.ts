1893318ff7cbceeb653454d6389eeb12d23d2f8c
ddolboghi <rhdfyd128@gmail.com>
2024-08-28 15:46:07 +0900

    fix: 데이터 구조 변경에 따른 타입 수정

--- utils/types.ts
+++ utils/types.ts
@@ -2,12 +2,5 @@ export type Todo = {
   topic: string;
   todoId: number;
   todo: string;
-  days: number[];
-};
-
-export type Completion = {
-  todoId: number;
-  complete: {
-    [key: string]: boolean;
-  };
+  days: { [key: string]: boolean };
 };
