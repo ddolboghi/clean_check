2c5f1594f094472820d3f234500a1d2a0e36be56
ddolboghi <rhdfyd128@gmail.com>
2024-08-29 04:57:26 +0900

    style: 로그인 페이지 애니메이션 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import cleanFreeLogo from "@/assets/cleanfreeLogo2.png";
 import Image from "next/image";
+import "@/style/loginPageAnimation.css";
 
 export default async function Login() {
   const {
@@ -19,11 +20,11 @@ export default async function Login() {
         <h1 className="font-semibold text-[40px] mb-6 opacity-0 translate-y-10 animate-fadeInUp">
           환영합니다!
         </h1>
-        <p className="text-[#808080] text-[20px] opacity-0 translate-y-10 animate-fadeIn delay-2000">
+        <p className="text-[#808080] text-[20px] opacity-0 translate-y-10 animate-fadeIn delay-1500">
           스킨체크와 함께 피부를 지켜보세요.
         </p>
       </div>
-      <div className="opacity-0 animate-fadeIn delay-2000">
+      <div className="opacity-0 animate-fadeIn delay-1500">
         <Image
           src={cleanFreeLogo}
           width={195}
@@ -31,7 +32,7 @@ export default async function Login() {
           alt="클린프리 로고"
         />
       </div>
-      <div className="opacity-0 animate-fadeIn delay-2000">
+      <div className="opacity-0 animate-fadeIn delay-1500">
         <GoogleLoginButton />
       </div>
     </main>
