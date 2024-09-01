6d65b2e5aadf6ade5bcbad8f3f552cff403cf873
ddolboghi <rhdfyd128@gmail.com>
2024-09-01 23:28:10 +0900

    fix: 체크리스트 생성 팝업 조건 수정

--- components/chat/Chatbot.tsx
+++ components/chat/Chatbot.tsx
@@ -125,7 +125,7 @@ export default function Chatbot() {
   return (
     <main>
       {/* 체크리스트 생성 모달로 띄워야 렌더링되면서 함수가 실행된다. */}
-      {!generatingCheckList && (
+      {generatingCheckList && (
         <GeneratingCheckList isGenerateCheckList={generatingCheckList} />
       )}
       {!closeResetPopup && (
