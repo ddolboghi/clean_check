67aa1cf691758e45dd22369091c6faaca6e79830
ddolboghi <rhdfyd128@gmail.com>
2024-09-06 15:35:43 +0900

    fix: 로고 이미지 경로 수정

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/../assets/cleanfreeLogo2.png",
+        src: "/assets/cleanfreeLogo2.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/../assets/cleanfreeLogo2.png",
+        src: "/assets/cleanfreeLogo2.png",
         sizes: "512x512",
         type: "image/png",
       },
