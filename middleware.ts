23fc5ad3b3be63872477d289bc9430038a805a51
ddolboghi <rhdfyd128@gmail.com>
2024-09-06 15:22:13 +0900

    fix: middleware user session refrash 주석 처리

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,9 @@ export async function middleware(request: NextRequest) {
   const response = await updateSession(request);
   const { pathname } = request.nextUrl;
 
-  if (pathname === "/auth/callback") {
-    await createClient().auth.getUser();
-  }
+  // if (pathname === "/auth/callback") {
+  //   await createClient().auth.getUser();
+  // }
 
   const {
     data: { user },
