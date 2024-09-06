4cd4860c48b4f274dc8dfc1abc893d0ee6d528a0
ddolboghi <rhdfyd128@gmail.com>
2024-09-06 15:35:24 +0900

    fix: static 파일 public으로 이동 및 import 경로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 
 const pretendard = localFont({
-  src: "../assets/fonts/PretendardVariable.woff2",
+  src: "../public/assets/fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
