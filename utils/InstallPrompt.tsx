b9b6ad8fbc37f1a7eab487183c2428e8f73da00f
ddolboghi <rhdfyd128@gmail.com>
2024-09-06 16:46:48 +0900

    refactor: 최상단 컴포넌트로 수정 및 문구 수정

--- utils/InstallPrompt.tsx
+++ utils/InstallPrompt.tsx
@@ -19,25 +19,29 @@ export default function InstallPrompt() {
   }
 
   return (
-    <div>
-      <h3>앱 설치하기</h3>
-      <button>홈화면에 앱 추가</button>
-      {isIOS && (
-        <p>
-          To install this app on your iOS device, tap the share button ios
-          기기에 앱을 설치하려면 공유 버튼을 누르세요.
-          <span role="img" aria-label="share icon">
-            {" "}
-            ⎋{" "}
-          </span>
-          그리고 "홈화면에 추가"를 누르세요.
-          <span role="img" aria-label="plus icon">
-            {" "}
-            ➕{" "}
-          </span>
-          .
-        </p>
-      )}
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white min-w-[300px]">
+        <h3>앱 설치하기</h3>
+        <button>홈화면에 앱 추가</button>
+        {isIOS ? (
+          <p>
+            기기에 앱을 설치하려면 공유 버튼을 누르세요.
+            <span role="img" aria-label="share icon">
+              {" "}
+              ⎋{" "}
+            </span>
+            그리고 "홈화면에 추가"를 누르세요.
+            <span role="img" aria-label="plus icon">
+              {" "}
+              ➕{" "}
+            </span>
+            .
+          </p>
+        ) : (
+          //안드로이드 설치 유도 수정하기
+          <p>주소창에서 다운로드 버튼을 클릭하세요.</p>
+        )}
+      </div>
     </div>
   );
 }
