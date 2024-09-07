e5edf3c7f99915c0975de4988831073a1c93f4dc
ddolboghi <rhdfyd128@gmail.com>
2024-09-07 10:09:51 +0900

    fix: primary key 제거

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,6 @@ self.addEventListener("push", function (event) {
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
-        primaryKey: "2",
       },
     };
     event.waitUntil(self.registration.showNotification(data.title, options));
