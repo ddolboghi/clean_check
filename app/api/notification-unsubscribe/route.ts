abfd4d171c96298ca351e3cdfa26a0e852a66881
ddolboghi <rhdfyd128@gmail.com>
2024-09-07 11:14:22 +0900

    refactor: 구독 삭제 로직 수정

--- app/api/notification-unsubscribe/route.ts
+++ app/api/notification-unsubscribe/route.ts
@@ -1,17 +1,20 @@
 import { createClient } from "@/utils/supabase/server";
+import { RequestDataType } from "@/utils/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { memberId } = await req.json();
+    const { memberId, pushSubscription }: RequestDataType = await req.json();
     const supabase = createClient();
 
     const { error } = await supabase
       .from("push_notification")
       .delete()
-      .eq("member_id", memberId);
+      .eq("member_id", memberId)
+      .eq("push_subscription", JSON.stringify(pushSubscription));
 
     if (error) {
+      console.log(error);
       return NextResponse.json({ message: error.message }, { status: 400 });
     } else {
       return NextResponse.json({ message: "success" }, { status: 200 });
