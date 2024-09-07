094eb6dc4c567fc03eb96228601438188c85f45b
ddolboghi <rhdfyd128@gmail.com>
2024-09-07 17:41:02 +0900

    fix: pwa 설정 수정

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,10 +4,11 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "스킨체크",
     short_name: "스킨체크",
-    description: "스킨체크 반응형 웹앱입니다.",
+    description: "스킨체크 웹앱입니다.",
     start_url: "/login",
     display: "standalone",
     theme_color: "#ffffff",
+    background_color: "#ffffff",
     icons: [
       {
         src: "/assets/cleanfreeLogoReversed.png",
