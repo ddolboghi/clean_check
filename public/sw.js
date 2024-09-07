655fd2c37d5965cbbd27eed5a4824ccea7c8a86e
ddolboghi <rhdfyd128@gmail.com>
2024-09-07 17:13:59 +0900

    fix: async-await 적용

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,8 @@
-self.addEventListener("push", function (event) {
-  if (event.data) {
-    const data = event.data.json();
-    const options = {
-      body: data.body,
-      icon: "/assets/cleanfreeLogoReversed.png",
-    };
-    event.waitUntil(self.registration.showNotification(data.title, options));
-  }
+self.addEventListener("push", async function (event) {
+  const data = await event.data.json();
+  const options = {
+    body: data.body,
+    // icon: "/assets/cleanfreeLogoReversed.png",
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
 });
