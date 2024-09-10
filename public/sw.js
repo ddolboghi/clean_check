47f71675919da8f944b38296b36f59eb8e1569a6
ddolboghi <rhdfyd128@gmail.com>
2024-09-10 11:59:28 +0900

    refactor: service worker 로직 및 기능 추가

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,20 @@
-self.addEventListener("push", async function (event) {
-  const data = await event.data.json();
-  const options = {
-    body: data.body,
-    // icon: "/assets/cleanfreeLogoReversed.png",
-  };
-  event.waitUntil(self.registration.showNotification(data.title, options));
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      // icon: "/assets/cleanfreeLogoReversed.png",
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  } else {
+    console.log("This push event has no data.");
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("Notification click received.");
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("https://skin-check-dev.vercel.app/checklist")
+  );
 });
