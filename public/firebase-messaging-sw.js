15c4e0f63a7ba21608d439370b11609c6cdcb54a
ddolboghi <rhdfyd128@gmail.com>
2024-09-10 20:38:57 +0900

    debug: firebase 설정 주석 처리

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,21 +1,21 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js"
-);
+// importScripts(
+//   "https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js"
+// );
+// importScripts(
+//   "https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js"
+// );
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCyYiX0wSuvYN0Fcj9X0uK_c5V61GLB8eY",
-  authDomain: "skin-check-2001e.firebaseapp.com",
-  projectId: "skin-check-2001e",
-  storageBucket: "skin-check-2001e.appspot.com",
-  messagingSenderId: "664553089796",
-  appId: "1:664553089796:web:27705746c0564b8f0f3e51",
-  measurementId: "G-Y838JL8F9P",
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyCyYiX0wSuvYN0Fcj9X0uK_c5V61GLB8eY",
+//   authDomain: "skin-check-2001e.firebaseapp.com",
+//   projectId: "skin-check-2001e",
+//   storageBucket: "skin-check-2001e.appspot.com",
+//   messagingSenderId: "664553089796",
+//   appId: "1:664553089796:web:27705746c0564b8f0f3e51",
+//   measurementId: "G-Y838JL8F9P",
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 self.addEventListener("push", function (event) {
   if (event.data) {
