01536c2ef81b91669bc48dff6940105df1e389e5
ddolboghi <rhdfyd128@gmail.com>
2024-09-12 22:32:05 +0900

    fix: content 내용 수정

--- components/chat/UserMessage.tsx
+++ components/chat/UserMessage.tsx
@@ -5,7 +5,7 @@ export default function UserMessage({ role, content }: ChatGptMessage) {
     return (
       <div className="flex justify-end">
         <div className="flex flex-col justify-center bg-[#E1F5F1] max-w-[283px] min-h-[60px] rounded-b-[20px] rounded-tl-[20px] rounded-tr-[5px]">
-          <p className="my-4 px-[18px]">{content.slice(9)}</p>
+          <p className="my-4 px-[18px]">{content}</p>
         </div>
       </div>
     );
