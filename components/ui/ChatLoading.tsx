c7f37a5a488faab3245f36e406ea2a96a63d3b0c
ddolboghi <rhdfyd128@gmail.com>
2024-09-14 00:39:30 +0900

    fix: 채팅 로딩 ui 아이콘 변경

--- components/ui/ChatLoading.tsx
+++ components/ui/ChatLoading.tsx
@@ -1,10 +1,10 @@
-import CleanFreeLogoGrad from "../icons/CleanFreeLogoGrad";
+import ChatbotIcon from "../icons/ChatbotIcon";
 
 export default function ChatLoading() {
   return (
     <div className="flex flex-row max-w-[283px] items-start">
       <div className="flex justify-center items-center rounded-full w-[40px] h-[40px] border-[#D2D2D2] border-[0.1px] flex-shrink-0">
-        <CleanFreeLogoGrad width="25" height="25" />
+        <ChatbotIcon />
       </div>
       <div className="flex flex-col justify-center bg-[#EEEEEE] max-w-[283px] min-w-[107px] min-h-[45px] rounded-b-[20px] rounded-tr-[20px] rounded-tl-[5px] ml-4 mt-[20px]">
         <div className="flex flex-row justify-center gap-[14px]">
