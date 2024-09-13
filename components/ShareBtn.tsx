1fcbbcd647bdc9671d858fc46db54e4dc16210d1
ddolboghi <rhdfyd128@gmail.com>
2024-09-13 13:52:09 +0900

    fix: 공유 메뉴 띄우도록 수정

--- components/ShareBtn.tsx
+++ components/ShareBtn.tsx
@@ -5,19 +5,28 @@ import { useState } from "react";
 import cleanFreeLogo2png from "@/public/assets/cleanfreeLogo2.png";
 
 export default function ShareBtn() {
-  const [imageUrl, setImageUrl] = useState("");
+  const [shareUrl] = useState(`${location.origin}${cleanFreeLogo2png}`);
 
-  const shareToInstagram = () => {
-    // 인스타그램 공유 링크로 이동
-    window.open(
-      `https://www.instagram.com/create/story/?url=${cleanFreeLogo2png}`,
-      "_blank"
-    );
+  const shareContent = async () => {
+    try {
+      if (
+        typeof window !== "undefined" &&
+        typeof window.navigator !== "undefined"
+      ) {
+        await navigator.share({
+          title: "인스타그램 스토리 공유",
+          text: "이 이미지를 확인해보세요!",
+          url: shareUrl,
+        });
+      }
+    } catch (error) {
+      console.error("공유 실패:", error);
+    }
   };
   return (
     <div>
       <Image src={cleanFreeLogo2png} alt="Preview" width={500} height={400} />
-      <button onClick={shareToInstagram}>인스타그램 스토리에 공유하기</button>
+      <button onClick={shareContent}>인스타그램 스토리에 공유하기</button>
     </div>
   );
 }
