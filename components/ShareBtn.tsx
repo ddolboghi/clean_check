1e0ea0614b17cbe255ff813e44c93ef7a7c9c308
ddolboghi <rhdfyd128@gmail.com>
2024-09-13 14:54:04 +0900

    feat: 파일 공유 기능으로 변경

--- components/ShareBtn.tsx
+++ components/ShareBtn.tsx
@@ -13,21 +13,51 @@ export default function ShareBtn() {
     }
   }, []);
 
-  const shareData = {
-    title: "인스타그램 스토리 공유",
-    text: "이 이미지를 확인해보세요!",
-    url: shareUrl!,
+  const copyImageToClipboard = async () => {
+    const img = document.createElement("img");
+    img.src = cleanFreeLogo2png.src;
+
+    img.onload = async () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+
+      if (ctx) {
+        ctx.drawImage(img, 0, 0);
+        canvas.toBlob(async (blob) => {
+          if (blob) {
+            await navigator.clipboard.write([
+              new ClipboardItem({
+                [blob.type]: blob,
+              }),
+            ]);
+            shareContent(blob);
+          }
+        });
+      }
+    };
   };
 
-  const shareContent = () => {
+  const shareContent = async (blob: Blob) => {
     try {
+      const file = new File([blob], "image.png", { type: blob.type });
+      const shareData = {
+        title: "인스타그램 스토리 공유",
+        text: "이 이미지를 확인해보세요!",
+        files: [file],
+      };
+
       if (
         typeof window !== "undefined" &&
         typeof window.navigator !== "undefined" &&
+        typeof navigator.share !== "undefined" &&
         shareUrl &&
         navigator.canShare(shareData)
       ) {
-        window.navigator.share(shareData);
+        await navigator.share(shareData);
+      } else {
+        throw new Error("This browser doesn't support sharing.");
       }
     } catch (error) {
       console.error("공유 실패:", error);
@@ -36,7 +66,9 @@ export default function ShareBtn() {
   return (
     <div>
       <Image src={cleanFreeLogo2png} alt="Preview" width={500} height={400} />
-      <button onClick={shareContent}>인스타그램 스토리에 공유하기</button>
+      <button onClick={copyImageToClipboard}>
+        인스타그램 스토리에 공유하기
+      </button>
     </div>
   );
 }
