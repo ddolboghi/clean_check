c6cca7040b92fefe2e3d7702ccf55deb0046d00a
ddolboghi <rhdfyd128@gmail.com>
2024-09-24 15:58:47 +0900

    refactor: keyword & semantic search 로직 적용

--- actions/search.ts
+++ actions/search.ts
@@ -1,6 +1,11 @@
 "use server";
 
 import { supabaseClient } from "@/lib/getSupabaseClient";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
 
 export interface SupabaseYoutube {
   id: number;
@@ -12,24 +17,29 @@ export interface SupabaseYoutube {
 
 export const getSearchResult = async (searchWord: string) => {
   const words = searchWord.split(/[\s,]+/);
+  words.push(searchWord);
   const formattedWords = words.map((word) => `'${word}'`).join(" | ");
   try {
-    // const { data, error } = await supabaseClient
-    //   .rpc("search_youtube", {
-    //     search_word: formattedWords,
-    //   })
-    //   .returns<SupabaseYoutube[]>();
+    const embeddingResponse = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: searchWord,
+      encoding_format: "float",
+      dimensions: 512,
+    });
 
-    const { data, error } = await supabaseClient
-      .from("youtube")
-      .select("*")
-      .textSearch("fts", formattedWords)
-      .returns<SupabaseYoutube[]>();
+    const [{ embedding }] = embeddingResponse.data;
+
+    const { data, error } = await supabaseClient.rpc("hybrid_search", {
+      query_text: searchWord,
+      query_embedding: embedding,
+      match_threshold: 0.5, // choose an appropriate threshold for your data
+      match_count: 10, // choose the number of matches
+    });
 
     if (error) throw error;
     if (!data || data.length === 0) throw new Error("Search result not exist.");
 
-    console.log(data, data[0].rank);
+    // console.log(data);
     return data;
   } catch (error) {
     console.error("[getSearchResult] Error:", error);
