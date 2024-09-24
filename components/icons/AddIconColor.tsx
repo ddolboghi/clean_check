607079a9f61feb81297356ae9269970893857939
ddolboghi <rhdfyd128@gmail.com>
2024-09-24 21:52:05 +0900

    refactor: width, height을 props로 받을 수 있도록 수정

--- components/icons/AddIconColor.tsx
+++ components/icons/AddIconColor.tsx
@@ -1,8 +1,13 @@
-export default function AddIconColor() {
+type AddIconColor = {
+  width?: string;
+  height?: string;
+};
+
+export default function AddIconColor({ width, height }: AddIconColor) {
   return (
     <svg
-      width="16"
-      height="16"
+      width={`${width ? width : "16"}`}
+      height={`${height ? height : "16"}`}
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
