8067a3d286436fda1955d82ee1095cf4ab156a31
ddolboghi <rhdfyd128@gmail.com>
2024-09-24 15:58:10 +0900

    refactor: 임베딩 데이터 저장

--- actions/youtube.ts
+++ actions/youtube.ts
@@ -2,22 +2,50 @@
 
 import { Youtube } from "@/components/youtube/AddYoutube";
 import { supabaseClient } from "@/lib/getSupabaseClient";
+import OpenAI from "openai";
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string;
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
 
 export const insertYoutubes = async (youtubes: Youtube[]) => {
   try {
     for (let youtube of youtubes) {
       const video = await getVideoData(youtube.videoId);
-
       if (!video) throw new Error("Video not exist.");
 
       const title = video.snippet.title;
+      const channelId = video.snippet.channelId;
+
+      const channel = await getChannelData(channelId);
+      if (!channel) throw new Error("Channel not exist.");
+
+      const channelName = channel.snippet.title;
+
+      let inputText =
+        channelName +
+        " " +
+        title +
+        " " +
+        Object.values(youtube.contents).join(" ");
+      inputText = inputText.replace(/[^a-zA-Z0-9가-힣\s]/g, "");
+
+      const response = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: inputText,
+        encoding_format: "float",
+        dimensions: 512,
+      });
+
+      const embedding = response.data[0].embedding;
+
       const { data, error } = await supabaseClient.from("youtube").insert([
         {
           video_id: youtube.videoId,
           title: title,
           contents: youtube.contents,
+          embedding: embedding,
         },
       ]);
 
