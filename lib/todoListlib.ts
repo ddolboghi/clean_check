7ea73d61b11b2b76c491205b507404a0841b3f21
ddolboghi <rhdfyd128@gmail.com>
2024-09-25 10:05:54 +0900

    refactor: 서비스 업데이트에 따른 사용하지 않는 파일 삭제

--- lib/todoListlib.ts
+++
@@ -1,7 +0,0 @@
-import { Todo } from "@/utils/types";
-
-export const getUniqueTopic = (todoList: Todo[]) => {
-  const uniqueTopics = Array.from(new Set(todoList.map((todo) => todo.topic)));
-  const newTopicList = ["전체", ...uniqueTopics];
-  return newTopicList;
-};
