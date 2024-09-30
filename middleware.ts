cf5c40a270d58090e99bd8fa6f3e3ad4f0b11ae4
ddolboghi <rhdfyd128@gmail.com>
2024-09-30 13:26:09 +0900

    fix: 로그인 인가 라우트 추가

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest) {
   } = await createClient().auth.getUser();
   // console.log("유저 정보 middleware: ", user);
 
-  const protectedRoutes = ["/main", "/search"];
+  const protectedRoutes = ["/main", "/search", "/storage"];
 
   const isLoggedIn = user !== null;
 
